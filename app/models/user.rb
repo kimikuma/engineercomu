d8a8d46a4d7d7f5dbd25d82660f2a9d8a6b0d55d
EC2 Default User <kimikuma>
2024-11-11 06:21:01 +0000

    更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   has_many :chats,dependent: :destroy
   has_many :posts,dependent: :destroy
   has_many :group_users,dependent: :destroy
-  has_many :rooms,through: :group_users
+  has_many :groups,through: :group_users
 
   has_one_attached :profile_image
 
